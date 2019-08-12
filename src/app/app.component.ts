44a9c91d93f81358b050943a3405b273ece3cfd8
Dennis Haegler <dennis.haegler@gmail.com>
2019-08-12 11:33:32 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inez-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'inez';
+}
